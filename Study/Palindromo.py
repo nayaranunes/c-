0927a6c3c2408aebd0ddbef9c548b9d557789271
nayaranunes <nnayara01123@gmail.com>
2024-05-11 19:55:31 +0800

    sabadou

---
+++ Study/Palindromo.py
@@ -0,0 +1,7 @@
+
+palavra = input("Digite uma palavra para verificar se é um palíndromo: ")
+palavra = palavra.lower()
+if palavra == palavra[::-1]:
+    print(f"A palavra '{palavra}' é um palíndromo.")
+else:
+    print(f"A palavra '{palavra}' não é um palíndromo.")
